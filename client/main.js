6ceb0406904a211356d01d51eb40d29b629855e1
José Nieto <jdnv.123@gmail.com>
2016-10-22 20:09:55 -0500

    Add sidebar structure to dashboard.html

--- client/main.js
+++ client/main.js
@@ -6,21 +6,21 @@ import '/imports/ui/templates.js';
 
 import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
-
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
-
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    Router.go('/login');
-    instance.counter.set(instance.counter.get() + 1);
-  },
-});
+// Template.hello.onCreated(function helloOnCreated() {
+//   // counter starts at 0
+//   this.counter = new ReactiveVar(0);
+// });
+//
+// Template.hello.helpers({
+//   counter() {
+//     return Template.instance().counter.get();
+//   },
+// });
+//
+// Template.hello.events({
+//   'click button'(event, instance) {
+//     // increment the counter when button is clicked
+//     Router.go('/login');
+//     instance.counter.set(instance.counter.get() + 1);
+//   },
+// });
