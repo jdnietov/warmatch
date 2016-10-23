aec5f74c25079973189bf67d68263b9dd1170686
José Nieto <jdnv.123@gmail.com>
2016-10-22 20:27:14 -0500

    Redirect home to dashboard when logged in

--- client/main.js
+++ client/main.js
@@ -1,10 +1,17 @@
+import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
+import '/imports/startup/accounts-config.js';
 import '/imports/startup/client';
 import '/imports/ui/templates.js';
 
 import './main.html';
+
+if(Meteor.userId()) {
+  console.log("Il y a des usagers");
+  Router.go('/dashboard');
+}
 
 // Template.hello.onCreated(function helloOnCreated() {
 //   // counter starts at 0
