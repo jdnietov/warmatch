5c488af550a62df460a050c6f0d2ebe713696926
José Nieto <jdnv.123@gmail.com>
2016-10-25 22:07:00 -0500

    Solve merge conflicts

--- client/dashboard/dashboard.js
+++ client/dashboard/dashboard.js
@@ -3,18 +3,8 @@ import { Tracker } from 'meteor/tracker';
 
 import '/imports/ui/matchFragment.js';
 import './dashboard.html';
-<<<<<<< HEAD
-
-/*Template.dashboard.onCreated( function dashboardOnCreated() {
-  Tracker.autorun(function() {
-    userRedirect('dashboard');
-
-});*/
-
 Template.dashboard.events({
   'click button'(event, instance) {
     Router.go('/createMatch');
   }
 });
-=======
->>>>>>> 287518a4b564cae584098b3fb0e8792d65e661a0
