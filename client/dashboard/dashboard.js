bf90ec3a3fe2c4ac5d33011e1130d3572a2de832
José Nieto <jdnv.123@gmail.com>
2016-10-26 14:04:51 -0500

    Change onClick and hover CSS, redirect from login

--- client/dashboard/dashboard.js
+++ client/dashboard/dashboard.js
@@ -4,6 +4,7 @@ import { Tracker } from 'meteor/tracker';
 import { OpenMatches } from '/imports/api/open-matches.js';
 import '/imports/ui/matchFragment.js';
 
+import './dashboard.css';
 import './dashboard.html';
 
 Template.dashboard.helpers({
