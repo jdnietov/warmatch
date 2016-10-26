dcb4e4ed49f5b1a850ffeb2223c5734697fa8b29
José Nieto <jdnv.123@gmail.com>
2016-10-26 13:51:52 -0500

    Show open matches data in matchFragment

--- imports/api/open-matches.js
+++ imports/api/open-matches.js
@@ -4,6 +4,4 @@ import { OpenMatchSchema } from '/imports/api/schemas.js';
 OpenMatch = new Mongo.Collection('openMatches');
 OpenMatch.attachSchema(OpenMatchSchema);
 
-console.log("exporting OpenMatches");
-
 export const OpenMatches = OpenMatch;
