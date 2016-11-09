530ebe7410d21dc78a1f47dda05f329ba5d0615c
Juanc0 <juanpos97@gmail.com>
2016-11-09 11:48:00 -0500

    sports

--- imports/api/sports.js
+++ imports/api/sports.js
@@ -1,7 +1,7 @@
 import { Mongo } from 'meteor/mongo';
 import { SportSchema } from '/imports/api/schemas.js';
 
-Sport = new Mongo.Collection('matches');
+Sport = new Mongo.Collection('sport');
 Sport.attachSchema(SportSchema);
 
 export const Sports = Sport;
