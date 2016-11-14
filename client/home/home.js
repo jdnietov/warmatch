6799f460707aa4f7a0e657433c1350a323f281a8
Raúl <rfcoralj@unal.edu.co>
2016-11-14 16:59:48 -0500

    Activando enlaces de Botones de inicio

--- client/home/home.js
+++ client/home/home.js
@@ -1,3 +1,14 @@
 import { Template } from 'meteor/templating';
 import { Tracker } from 'meteor/tracker';
 import './home.html';
+
+Template.home.events({
+  'click #login'(event){
+    event.preventDefault();
+    Router.go('/login');
+  },
+  'click #register'(event){
+    event.preventDefault();
+    Router.go('/register');
+  }
+});
