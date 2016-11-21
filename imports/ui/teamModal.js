c8e2149874799861c0aa3f3c800efdf130afb07d
Juan Pablo Ovalle Sánchez <juanpos97@gmail.com>
2016-11-21 11:47:14 -0500

    add import to teamModal

--- imports/ui/teamModal.js
+++ imports/ui/teamModal.js
@@ -1,5 +1,6 @@
 import { Template } from 'meteor/templating';
 import { Teams } from '/imports/api/teams.js';
+import { RegisterTURs } from '/imports/api/registerTURs';
 
 import './teamModal.css';
 import './teamModal.html';
@@ -24,22 +25,14 @@ Template.teamModal.helpers({
 });
 
 Template.teamModal.events({
-	'click #createTeam': function(event, instance){
-    const target = event.target;
-		console.log(taget.name.value);
-	},
 	'submit #teamForm'(event, instance) {
-		Teams.insert({
-      name: target.name.value,
-			description: target.description.value,
-      sport: target.sport.value,
-			logo: Session.get("fileId"),
-			statistics: [
-				{name: "Partidos jugados", value: 0},
-				{name: "Partidos ganados", value: 0},
-				{name: "Partidos perdidos", value: 0},
-				{name: "Partidos empatados", value: 0}
-			]
-    });
+			const target = event.target;
+			console.log(target.name.value);
+			RegisterTURs.insert({
+				teamName: target.name.value,
+				userName: Meteor.user().username,
+				roleName: "Administrador"
+			})
+	    console.log("RegisterTURs Inserted!");
   }
 });
