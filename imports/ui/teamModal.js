fd485ce2f59def1ec4c5b2776317404a4882d79c
Juan Pablo Ovalle Sánchez <juanpos97@gmail.com>
2016-11-23 08:21:37 -0500

    Created teamModal

--- imports/ui/teamModal.js
+++ imports/ui/teamModal.js
@@ -1,42 +1,48 @@
 import { Template } from 'meteor/templating';
 import { Teams } from '/imports/api/teams.js';
 import { RegisterTURs } from '/imports/api/registerTURs';
+import { ImagesCol } from '/imports/api/images.js';
 
 import './teamModal.css';
 import './teamModal.html';
 
-Template.teamModal.helpers({
-	statisticsList: function(){
-		var ret = [
-			{name: "Partidos jugados", value: 0},
-			{name: "Partidos ganados", value: 0},
-			{name: "Partidos perdidos", value: 0},
-			{name: "Partidos empatados", value: 0}
-		];
-		console.log(ret);
-		return ret;
-	},
-  sportsOptions: () => {
-		var ret = [
-			{label: "Fútbol", value: "Fútbol"},
-			{label: "Baloncesto", value: "Baloncesto"},
-			{label: "Béisbol", value: "Béisbol"},
-			{label: "Vóleibol", value: "Vóleibol"}
-		];
-		console.log(ret);
-    return ret;
-  }
-});
-
 Template.teamModal.events({
-	'submit #teamForm'(event, instance) {
-			const target = event.target;
-			console.log(target.name.value);
-			RegisterTURs.insert({
-				teamName: target.name.value,
-				userName: Meteor.user().username,
-				roleName: "Administrador"
-			})
-	    console.log("RegisterTURs Inserted!");
-  }
+	'change .myFileInput': function(event, template) {
+    if(Session.get('fileId')){
+      ImagesCol.remove({_id:Session.get('fileId')}, true);
+    }
+    FS.Utility.eachFile(event, function(file) {
+      ImagesCol.insert(file, function (err, fileObj) {
+        var fileId = fileObj._id;
+        Session.set('fileId', fileId);
+      });
+    });
+  },
+  'submit #teamForm'(event, instance) {
+    event.preventDefault();
+    const target = event.target;
+		console.log("asd");
+    Teams.insert({
+      name: target.name.value,
+			description: target.description.value,
+      sport: target.sport.value,
+			logo: Session.get("fileId"),
+			statistics: [
+				{name: "Partidos jugados", value: 0},
+				{name: "Partidos ganados", value: 0},
+				{name: "Partidos perdidos", value: 0},
+				{name: "Partidos empatados", value: 0}
+			],
+      requests: []
+    });
+
+		RegisterTURs.insert({
+			teamName: target.name.value,
+			userName: Meteor.user().username,
+			roleName: "Administrador"
+		})
+
+    Session.set("fileId",undefined);
+		Modal.hide('teamModal');
+	}
 });
